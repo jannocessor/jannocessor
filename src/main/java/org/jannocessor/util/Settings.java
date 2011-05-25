68a252aff53e33b5c3bd08d3ce1373b1d8d40473
Nikolche Mihajlovski <nikolce.mihajlovski@gmail.com>
2011-05-25 23:02:01 +0200

    Added settings constants.

---
+++ src/main/java/org/jannocessor/util/Settings.java
@@ -0,0 +1,11 @@
+package org.jannocessor.util;
+
+public interface Settings {
+
+	public static final String GENERAL_PROPERTIES = "jannocessor.properties";
+
+	public static final String ANNOTATIONS_PROPERTIES = "annotations.properties";
+
+	public static final String KNOLWEDGE_BASE = "knolwedge_base.bin";
+
+}
