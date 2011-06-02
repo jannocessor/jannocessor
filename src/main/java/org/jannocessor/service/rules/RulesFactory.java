cf2cb0c4e436ab036d7103d6671982a8af6d5f5d
Nikolche Mihajlovski <nikolce.mihajlovski@gmail.com>
2011-06-02 08:18:48 +0200

    Implemented Rules service.

---
+++ src/main/java/org/jannocessor/service/rules/RulesFactory.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2011 jannocessor.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jannocessor.service.rules;
+
+import org.jannocessor.service.api.Configuratîr;
+import org.jannocessor.service.api.JannocessorException;
+import org.jannocessor.service.api.PathLocator;
+import org.jannocessor.service.api.RuleExecutor;
+
+public class RulesFactory {
+
+	public static RuleExecutor createRulesService(Configuratîr config,
+			PathLocator paths) throws JannocessorException {
+		return new RulesServiceImpl(config, paths);
+	}
+
+}
