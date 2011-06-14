a52e21348bf6bfe99d1ecad6562dd81a5cb746ed
Nikolche Mihajlovski <nikolce.mihajlovski@gmail.com>
2011-06-14 19:59:00 +0200

    Bootstrapped annotation processing test.

---
+++ src/test/java/org/jannocessor/bootstrapped/model/PersonModel.java
@@ -0,0 +1,20 @@
+package org.jannocessor.bootstrapped.model;
+
+import java.util.Date;
+
+import org.jannocessor.bootstrapped.annotation.MyModel;
+
+
+
+@MyModel
+public class PersonModel {
+
+	String name;
+
+	int age;
+
+	Date birthday;
+
+	java.sql.Date smartImportTestDate;
+
+}
