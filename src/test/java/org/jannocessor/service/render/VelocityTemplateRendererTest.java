a2b366e4623ba6547403956d3491563a6c6a867e
Nikolche Mihajlovski <nikolce.mihajlovski@gmail.com>
2011-08-30 12:58:42 +0200

    Added unit test for Velocity template renderer.

---
+++ src/test/java/org/jannocessor/service/render/VelocityTemplateRendererTest.java
@@ -0,0 +1,36 @@
+package org.jannocessor.service.render;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jannocessor.service.api.JannocessorException;
+import org.jannocessor.service.api.TemplateRenderer;
+import org.jannocessor.test.AbstractTest;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VelocityTemplateRendererTest extends AbstractTest {
+
+	private TemplateRenderer renderer;
+
+	@Before
+	public void initialize() {
+		renderer = new VelocityTemplateRenderer(null);
+	}
+
+	@Test
+	public void testRender() throws JannocessorException {
+		String template = "x=$x, foo=$foo, $other";
+
+		Map<String, Object> attributes = new HashMap<String, Object>();
+		attributes.put("x", 1);
+		attributes.put("foo", "bar");
+
+		String text = renderer.render(template, attributes);
+
+		assertEquals("x=1, foo=bar, $other", text);
+	}
+
+}
