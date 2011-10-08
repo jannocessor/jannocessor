5ceb129df7d2514a715e9035dd8beb669306f507
Nikolche Mihajlovski <nikolce.mihajlovski@gmail.com>
2011-10-09 00:44:18 +0200

    Changed copyright notice.

--- jannocessor-generated/src-generated/main/java/org/jannocessor/data/JavaFieldData.java
+++ jannocessor-generated/src-generated/main/java/org/jannocessor/data/JavaFieldData.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2011 jannocessor.org
+ * Copyright 2011 Nikolche Mihajlovski
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
