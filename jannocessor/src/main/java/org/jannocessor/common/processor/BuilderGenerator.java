c29e170f37f6e6b55329e49163233a54e3224ce4
Nikolche Mihajlovski <nikolce.mihajlovski@gmail.com>
2011-10-31 23:07:52 +0100

    Added common code processors for builder and DTO generation.

---
+++ jannocessor/src/main/java/org/jannocessor/common/processor/BuilderGenerator.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright 2011 Nikolche Mihajlovski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jannocessor.common.processor;
+
+import javax.annotation.Generated;
+
+import org.jannocessor.collection.Power;
+import org.jannocessor.collection.api.PowerList;
+import org.jannocessor.model.executable.JavaMethod;
+import org.jannocessor.model.structure.JavaClass;
+import org.jannocessor.model.structure.JavaMetadata;
+import org.jannocessor.model.type.JavaType;
+import org.jannocessor.model.type.JavaTypeKind;
+import org.jannocessor.model.util.Methods;
+import org.jannocessor.model.util.New;
+import org.jannocessor.model.variable.JavaField;
+import org.jannocessor.model.variable.JavaParameter;
+import org.jannocessor.processor.context.CodeProcessor;
+import org.jannocessor.processor.context.ProcessingContext;
+
+public class BuilderGenerator implements CodeProcessor<JavaClass> {
+
+	private final boolean inDebugMode;
+
+	public BuilderGenerator(boolean inDebugMode) {
+		this.inDebugMode = inDebugMode;
+	}
+
+	/**
+	 * Processes a list of annotated domain model classes (e.g. Person),
+	 * transforming each of them to a new class that represents a builder (e.g.
+	 * PersonBuilder). Finally, the source code of the transformed classes is
+	 * generated.
+	 */
+	public void process(PowerList<JavaClass> classes, ProcessingContext context) {
+		for (final JavaClass clazz : classes) {
+			// the name of the domain model class (e.g. Person)
+			String modelName = clazz.getName().getText();
+
+			// construct the builder name (e.g. PersonBuilder)
+			clazz.getName().appendPart("Builder");
+
+			// the type representation of the builder class (e.g. PersonBuilder type)
+			JavaType builderType = New.type(modelName + "Builder", JavaTypeKind.DECLARED);
+
+			// initialize empty list of methods for the builder
+			PowerList<JavaMethod> methods = Power.list();
+
+			// iterate the model fields (e.g. firstName, lastName... in Person)
+			for (JavaField field : clazz.getFields()) {
+				// the field name, e.g. firstName
+				String fieldName = field.getName().getText();
+
+				// a parameter for the new method in the builder (e.g. String firstName)
+				JavaParameter param1 = New.parameter(field.getType(), fieldName);
+
+				// create new method names as the field
+				// e.g. public PersonBuilder firstName(String firstName) {...}
+				JavaMethod method = New.method(Methods.PUBLIC, builderType, fieldName, param1);
+
+				// specify the method body source code (assigns the param value to the field)
+				// e.g. this.firstName = firstName
+				String body = String.format("this.%s = %s;\nreturn this;", fieldName, fieldName);
+				method.getBody().setHardcoded(body);
+
+				// add the method to the builder methods list
+				methods.add(method);
+			}
+
+			// create the builder method that constructs the model
+			// e.g. public Person build() {...)
+			JavaMethod build = New.method(Methods.PUBLIC, clazz.getType(), "build");
+
+			// the method body is non-trivial, so it will be rendered from the build.vm template
+			build.getBody().setTemplateName("build");
+
+			// add the "build" method to the list of methods
+			methods.add(build);
+
+			// remove the model class constructors
+			clazz.getConstructors().clear();
+
+			// assign the previously created methods to the builder class
+			clazz.getMethods().assign(methods);
+
+			// annotate the generated class as: @Generated("Easily by JAnnocessor :)")
+			JavaMetadata metadata = New.metadata(Generated.class, "Easily by JAnnocessor :)");
+			clazz.getMetadata().assign(Power.list(metadata));
+
+			// finally, generated the builder source code (e.g. PersonBuilder.java)
+			// if inDebugMode was set to true, the JAnnocessor UI will be displayed
+			context.generateCode(clazz, inDebugMode);
+		}
+	}
+}
---
+++ jannocessor/src/main/java/org/jannocessor/common/processor/DtoGenerator.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2011 Nikolche Mihajlovski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jannocessor.common.processor;
+
+import org.jannocessor.collection.api.PowerList;
+import org.jannocessor.common.annotation.DtoIgnore;
+import org.jannocessor.model.executable.JavaMethod;
+import org.jannocessor.model.structure.JavaClass;
+import org.jannocessor.model.structure.JavaMetadata;
+import org.jannocessor.model.util.Annotations;
+import org.jannocessor.model.util.Classes;
+import org.jannocessor.model.util.Methods;
+import org.jannocessor.model.util.New;
+import org.jannocessor.model.variable.JavaField;
+import org.jannocessor.processor.context.CodeProcessor;
+import org.jannocessor.processor.context.ProcessingContext;
+
+public class DtoGenerator implements CodeProcessor<JavaClass> {
+
+	private final boolean inDebugMode;
+
+	public DtoGenerator(boolean inDebugMode) {
+		this.inDebugMode = inDebugMode;
+	}
+
+	/**
+	 * Processes a list of annotated domain model classes (e.g. Person),
+	 * generating a Domain Transfer Object (DTO) class for each of them.
+	 */
+	public void process(PowerList<JavaClass> classes, ProcessingContext context) {
+		final JavaMetadata ignore = New.metadata(DtoIgnore.class);
+
+		for (JavaClass model : classes) {
+			JavaClass dto = New.classs(Classes.PUBLIC, model.getName() + "Dto");
+
+			// iterate the model fields (e.g. firstName, lastName... in Person)
+			for (JavaField field : model.getFields()) {
+				if (!field.getMetadata().contains(ignore)) {
+					JavaMethod getter = Methods.getter(field);
+					JavaMethod setter = Methods.setter(field);
+
+					dto.getMethods().addAll(getter, setter);
+
+					field.getMetadata().clear();
+					dto.getFields().add(field);
+				}
+			}
+
+			dto.getMetadata().add(Annotations.generated("JAnnocessor"));
+
+			New.packagee("a.b").getClasses().add(dto);
+			context.generateCode(dto, inDebugMode);
+		}
+	}
+}
