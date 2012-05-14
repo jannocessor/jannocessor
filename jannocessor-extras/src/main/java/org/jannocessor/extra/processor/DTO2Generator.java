034d0b55c222769b228f35914ae5601043821881
Nikolche Mihajlovski <nikolce.mihajlovski@gmail.com>
2012-05-14 06:40:12 +0200

    Renaming DtoGenerator to DTOGenerator.

---
+++ jannocessor-extras/src/main/java/org/jannocessor/extra/processor/DTO2Generator.java
@@ -0,0 +1,44 @@
+package org.jannocessor.extra.processor;
+
+import org.jannocessor.collection.api.PowerList;
+import org.jannocessor.model.executable.JavaMethod;
+import org.jannocessor.model.structure.AbstractJavaClass;
+import org.jannocessor.model.structure.JavaClass;
+import org.jannocessor.model.type.JavaType;
+import org.jannocessor.model.util.Classes;
+import org.jannocessor.model.util.Methods;
+import org.jannocessor.model.util.New;
+import org.jannocessor.model.variable.JavaField;
+import org.jannocessor.processor.api.ProcessingContext;
+
+public class DTO2Generator extends AbstractGenerator<AbstractJavaClass> {
+
+	public DTO2Generator(String destPackage, boolean inDebugMode) {
+		super(destPackage, inDebugMode);
+	}
+
+	/**
+	 * Generates a DTO class from the specified model.
+	 */
+	protected void generateCodeFrom(PowerList<AbstractJavaClass> models, ProcessingContext context) {
+		for (AbstractJavaClass model : models) {
+			JavaClass dto = New.classs(Classes.PUBLIC, model.getName() + "DTO");
+
+			// iterate the model fields (e.g. firstName, lastName... in Person)
+			for (JavaField f : model.getFields()) {
+				JavaType type = replaceModel(f.getType(), getDestPackage() + ".", "DTO");
+				JavaField field = New.field(type, f.getName().toString());
+
+				JavaMethod getter = Methods.getter(field);
+				JavaMethod setter = Methods.setter(field);
+
+				dto.getMethods().addAll(getter, setter);
+
+				field.getMetadata().clear();
+				dto.getFields().add(field);
+			}
+
+			generate(dto);
+		}
+	}
+}
